bf906a3e6e4aa3b65eed3581d0733cc32edbdac0
Onur Arıkan <onur93arikan@gmail.com>
2022-11-26 18:17:08 +0300

    updated with 2022 results

--- __main__.py
+++ __main__.py
@@ -1,21 +1,27 @@
-import json
 from collections import defaultdict
+import json
 from statistics import mean
+
 import pandas as pd
 
-def elo(rating1, rating2, vs1, vs2, K = 200):
-    if vs1 + vs2 <= 0: 
-        return 0 
-    
-    expected = 1 / (1 + 10**((rating2 - rating1)/400))
+
+def elo(rating1, rating2, vs1, vs2, K=200):
+    if vs1 + vs2 <= 0:
+        return 0
+    expected = 1 / (1 + 10 ** ((rating2 - rating1) / 400))
     score = round(vs1 / (vs1 + vs2), 2)
-    
-    if   0    <= score <  0.25: point = 0
-    elif 0.25 <= score <= 0.40: point = 0.3
-    elif 0.40 <  score <  0.60: point = 0.5
-    elif 0.60 <= score <= 0.75: point = 0.7
-    elif 0.75 <  score <= 1:    point = 1
+    if 0 <= score < 0.25:
+        point = 0
+    elif 0.25 <= score <= 0.40:
+        point = 0.3
+    elif 0.40 < score < 0.60:
+        point = 0.5
+    elif 0.60 <= score <= 0.75:
+        point = 0.7
+    elif 0.75 < score <= 1:
+        point = 1
     return round(K * (point - expected))
+
 
 if __name__ == "__main__":
     firstYear = json.load(open("firstYear.json"))
@@ -31,15 +37,15 @@ if __name__ == "__main__":
             gain = elo(rating1, rating2, row.VS1, row.VS2)
             helperDict[row.DRIVER1].append(gain)
             helperDict[row.DRIVER2].append(-gain)
-        
+
         for driver, gainList in helperDict.items():
             driverPoints[driver] += round(mean(gainList))
-        
+
         df[year] = [(point if year >= firstYear[driver] else 0) for driver, point in driverPoints.items()]
-                
+
     df["PUAN"] = df[year]
-    df = df.sort_values("PUAN",ascending=False)
-    df["MAX"] = df.iloc[:,4:].max(axis=1)
+    df = df.sort_values("PUAN", ascending=False)
+    df["MAX"] = df.iloc[:, 4:].max(axis=1)
     df = df[["PİLOT", "PUAN", "İLK SEZON", "MAX", *range(year, 1949, -1)]]
 
     df.to_excel(f'elo{year}.xlsx', sheet_name='PUANLAR', columns=df.columns, index=False)
