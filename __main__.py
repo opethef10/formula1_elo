5df3ae7aaecf83fba97281ee78ddd41e7cff77b8
Onur Arıkan <onur93arikan@gmail.com>
2025-12-07 20:40:15 +0100

    Refactor

--- __main__.py
+++ __main__.py
@@ -4,9 +4,11 @@ from statistics import mean
 
 import pandas as pd
 
+FIRST_YEAR = 1950
+
 
 def elo(rating1, rating2, vs1, vs2, K=200):
-    if vs1 + vs2 <= 0:
+    if vs1 < 0 or vs2 < 0 or vs1 + vs2 <= 0:
         return 0
     expected = 1 / (1 + 10 ** ((rating2 - rating1) / 400))
     score = round(vs1 / (vs1 + vs2), 2)
@@ -20,6 +22,8 @@ def elo(rating1, rating2, vs1, vs2, K=200):
         point = 0.7
     elif 0.75 < score <= 1:
         point = 1
+    else:
+        return 0
     return round(K * (point - expected))
 
 
@@ -27,9 +31,10 @@ if __name__ == "__main__":
     firstYear = json.load(open("firstYear.json"))
     driverPoints = json.load(open("driverPoints.json"))
 
-    df = pd.DataFrame(firstYear.items(), columns=['PİLOT', 'İLK SEZON'])
+    df = pd.DataFrame(firstYear.items(), columns=['DRIVER', 'FIRST YEAR'])
 
-    for year, seasonTable in pd.read_csv("qualifying_battles.csv").groupby("YIL"):
+    year = FIRST_YEAR
+    for year, seasonTable in pd.read_csv("qualifying_battles.csv").groupby("YEAR"):
         helperDict = defaultdict(list)
         for row in seasonTable.itertuples():
             rating1 = driverPoints[row.DRIVER1]
@@ -41,11 +46,14 @@ if __name__ == "__main__":
         for driver, gainList in helperDict.items():
             driverPoints[driver] += round(mean(gainList))
 
-        df[year] = [(point if year >= firstYear[driver] else 0) for driver, point in driverPoints.items()]
+        df[year] = [
+            (point if year >= firstYear[driver] else 0)
+            for driver, point in driverPoints.items()
+        ]
 
-    df["PUAN"] = df[year]
-    df = df.sort_values("PUAN", ascending=False)
+    df["POINTS"] = df[year]
+    df = df.sort_values("POINTS", ascending=False)
     df["MAX"] = df.iloc[:, 4:].max(axis=1)
-    df = df[["PİLOT", "PUAN", "İLK SEZON", "MAX", *range(year, 1949, -1)]]
+    df = df[["DRIVER", "POINTS", "FIRST YEAR", "MAX", *range(year, FIRST_YEAR - 1, -1)]]
 
-    df.to_excel(f'elo{year}.xlsx', sheet_name='PUANLAR', columns=df.columns, index=False)
+    df.to_excel(f'elo{year}.xlsx', sheet_name='POINTS', columns=df.columns, index=False)
